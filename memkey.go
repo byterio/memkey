e7f9b5095e85033d38a4c7207c92a1e1413a8f13
itpey <52120725+itpey@users.noreply.github.com>
2026-06-24 18:51:31 +0330

    Update memkey.go

--- memkey.go
+++ memkey.go
@@ -1,4 +1,4 @@
-// Copyright 2025 Byterio
+// Copyright 2026 Byterio
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
